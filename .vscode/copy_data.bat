7abbd725a79abca2f1ee115a538b9a057b642829
slavashestakov2005 <slavashestakov2005@gmail.com>
2024-07-03 20:41:39 +0700

    Обновление и сохранение данных

---
+++ .vscode/copy_data.bat
@@ -0,0 +1,18 @@
+if not exist ..\\ITI-data (
+    ..\\ITI-data
+    cd ..
+    cd ITI-data
+    git init
+    cd ..
+    cd ITI
+)
+
+if exist ..\\ITI-data\\data rmdir /s /q ..\\ITI-data\\data
+if exist ..\\ITI-data\\templates rmdir /s /q ..\\ITI-data\\templates
+mkdir ..\\ITI-data\\data
+mkdir ..\\ITI-data\\templates
+
+copy .env ..\\ITI-data\\.env
+copy backend\\database.db ..\\ITI-data\\database.db
+xcopy /s /e backend\\data ..\\ITI-data\\data
+xcopy /s /e backend\\templates ..\\ITI-data\\templates
