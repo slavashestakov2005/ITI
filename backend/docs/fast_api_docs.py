379115b99951fe80f75fbb8de1485c5f2f7007fd
slavashestakov2005 <slavashestakov2005@gmail.com>
2026-02-01 16:35:45 +0300

    Fix linter

--- backend/docs/fast_api_docs.py
+++ backend/docs/fast_api_docs.py
@@ -10,5 +10,5 @@ sys.path.insert(0, os.path.abspath("../../backend"))
 from main import app  # noqa: E402
 
 openapi_data = app.openapi()
-with open("openapi.json", "w") as f:
-    json.dump(openapi_data, f, indent=2)
+with open("openapi.json", "w") as api_file:
+    json.dump(openapi_data, api_file, indent=2)
